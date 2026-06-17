93fef44ade8620020ceaf0d788bed1ed5e618b4c
GiovaneHernandes <143449961+GiovaneHernandes@users.noreply.github.com>
2026-06-17 08:07:29 -0300

    Retira import Player

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Player from "../components/Player";
 import Programacaocard from "../components/Programacaocard";
 import Top10 from "../components/Top10";
 import Footer from "../components/Footer";
